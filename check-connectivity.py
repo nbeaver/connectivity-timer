7bc349aa5ddf946d1c1180989f08e3065ba0f516
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-03-13 17:31:26 -0500

    Debugging, mostly.

--- check-connectivity.py
+++ check-connectivity.py
@@ -7,23 +7,33 @@ url = 'http://www.python.org'
 loop_time = 1 # seconds
 time_of_last_success = -1
 time_of_last_failure = -1
-verbose = False
+verbose = True
 successful_last_time = -1
 while True:
     try:
         f = urllib2.urlopen(url, data=None, timeout=0.5)
+        print "Got past urllib2!"
     except urllib2.URLError:
         now = time.time()
-        if verbose: print "Cannot resolve url",url
+        if verbose:
+            print "Cannot resolve url",url
+            if time_of_last_success != -1 and time_of_last_failure != -1:
+                print "Connection has failed for this long:", now - time_of_last_success,"+/-",loop_time
         # The connection worked, so let's see if it failed last time. If so, let's see how long it was down.
         if time_of_last_success != -1 and time_of_last_failure != -1 and successful_last_time == True:
             print "Connection failed for this long:", now - time_of_last_success,"+/-",loop_time
         time_of_last_failure = now
         successful_last_time = False
         pass
+    except:
+        print "A different error."
+        raise
     else:
         now = time.time()
-        if verbose: print "Successfully resolved url", url
+        if verbose:
+            print "Successfully resolved url", url
+            if time_of_last_success != -1 and time_of_last_failure != -1:
+                print "Connection has worked for this long:",now - time_of_last_failure,"+/-",loop_time
         # The connection failed, so let's see if it worked last time. If so, let's see how long it lasted.
         if time_of_last_success != -1 and time_of_last_failure != -1 and successful_last_time == False:
             print "Connection worked for this long:",now - time_of_last_failure,"+/-",loop_time
