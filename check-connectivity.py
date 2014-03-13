65a7ff54ea26d3178b3e3a47fc0f6172d629b9da
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-03-13 14:46:01 -0500

    Added counters to track how long the connection works for.

--- check-connectivity.py
+++ check-connectivity.py
@@ -5,13 +5,22 @@ import urllib2
 import time
 url = 'http://www.python.org'
 loop_time = 1 # seconds
+time_of_last_success = -1
+time_of_last_failure = -1
+verbose = False
 while True:
     try:
         f = urllib2.urlopen(url, data=None, timeout=0.5)
     except urllib2.URLError:
-        print "Cannot resolve url",url
+        time_of_last_failure = time.time()
+        if verbose: print "Cannot resolve url",url
+        if time_of_last_success != -1 and time_of_last_failure != -1:
+            print "Connection has failed for this long:", time_of_last_failure - time_of_last_success
         pass
     else:
-        print "Successfully resolved url", url
+        time_of_last_success = time.time()
+        if verbose: print "Successfully resolved url", url
+        if time_of_last_success != -1 and time_of_last_failure != -1:
+            print "Connection has worked for this long:",time_of_last_success - time_of_last_failure
     finally:
         time.sleep(loop_time)
