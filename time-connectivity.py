6542cdf8c767a9235e9a5d2a9bb8fd1c126b298f
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2014-10-10 15:46:19 -0500

    Added todo.

--- time-connectivity.py
+++ time-connectivity.py
@@ -7,6 +7,7 @@ import socket
 # http://docs.python.org/2.7/library/socket.html#socket.socket.settimeout
 url = 'http://www.google.com' #TODO: make a command flag for this
 loop_time = 0.5 # seconds #TODO: make a command flag for this
+# TODO: make global timeout wait forever by default.
 global_timeout = 100 # seconds #TODO: make a command flag for this
 time_of_last_success = None
 time_of_last_failure = None
