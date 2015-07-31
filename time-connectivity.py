9e7fad01175aee0a40c4f1348f9e6efbed2480bb
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-07-31 17:34:20 -0500

    Refactor and cleanup.

--- time-connectivity.py
+++ time-connectivity.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python2.7
-# http://docs.python.org/2/library/urllib2.html#urllib2.urlopen
 import urllib2
-# http://docs.python.org/2/library/time.html#time.sleep
 import time
 import socket
 import httplib
 
-# For catching Ctrl-C.
-import signal
+import signal # For catching Ctrl-C.
 import sys
 
-def print_with_time(in_string):
-    print time.asctime() + " " + in_string
+def prepend_time(in_string):
+    return time.asctime() + " " + in_string
 
 def exit_cleanly(signal_number, stack_frame):
     # TODO: show how long it's been up or down at this point, too,
@@ -36,20 +33,21 @@ verbose = False #TODO: make a command flag for this
 successful_last_time = False
 socket.setdefaulttimeout(global_timeout)
 start_time = time.time()
-print "Starting timer."
+sys.stdout.write("Starting timer.\n")
+sys.stdout.flush()
 while True: # have to end the program manually. TODO: make a keypress like Ctrl-D end the program
     try:
         response = urllib2.urlopen(url, data=None, timeout=global_timeout)
     except socket.timeout:
-        print "Socket timed out!"
-        raise
+        sys.stdout("Socket timed out.\n")
+        pass
     except socket.error:
-        print "Socket error, not stopping."
-        #TODO: print information on the kind of socket error.
+        sys.stdout("Socket error, not stopping.\n")
+        # TODO: print information on the kind of socket error.
         pass
     except httplib.BadStatusLine:
-        print "httplib:BadStatusLine, not stopping."
-        #TODO: print information on the kind of HTTP error.
+        sys.stdout("httplib: BadStatusLine, not stopping.\n")
+        # TODO: print information on the kind of HTTP error.
         pass
     except urllib2.URLError:
         now = time.time()
@@ -58,12 +56,12 @@ while True: # have to end the program manually. TODO: make a keypress like Ctrl-
         else:
             running_failure_length = now - start_time
         if verbose:
-            print "Cannot resolve url",url
+            sys.stdout.write("Cannot resolve url: {0}\n".format(url))
             if running_failure_length:
-                print_with_time("Connection has failed for this long: "+ str(running_failure_length) + "+/-" + str(loop_time) + " seconds.")
+                sys.stdout.write(prepend_time("Connection has failed for this long: {0}+/-{1} seconds.\n".format(running_failure_length, loop_time)))
         # The connection failed, so let's see if it worked last time. If so, let's see how long it was up.
         if running_failure_length and successful_last_time == True:
-            print_with_time("Connection was up for this long: "+str(running_connection_length)+"+/-"+str(loop_time) + " seconds.")
+            sys.stdout.write(prepend_time("Connection was up for this long: {0}+/-{1} seconds.\n".format(running_connection_length, loop_time)))
         time_of_last_failure = now
         successful_last_time = False
         running_connection_length = None
@@ -75,12 +73,12 @@ while True: # have to end the program manually. TODO: make a keypress like Ctrl-
         else:
             running_connection_length = now - start_time
         if verbose:
-            print "Successfully resolved url", url
-            print_with_time("Connection has worked for this long: "+str(running_connection_length)+"+/-"+str(loop_time) + " seconds.")
+            sys.stdout.write("Successfully resolved url: {0}\n".format(url))
+            sys.stdout.write(prepend_time("Connection has worked for this long: {0}+/-{1} seconds.\n".format(running_connection_length, loop_time)))
         # The connection worked, so let's see if it failed last time.
         # If so, let's see how long it was down.
         if running_failure_length and successful_last_time == False:
-            print_with_time("Connection failed for this long: "+str(running_failure_length)+"+/-"+str(loop_time) + " seconds.")
+            sys.stdout.write(prepend_time("Connection failed for this long: {0}+/-{1} seconds.\n".format(running_failure_length, loop_time)))
         # Don't include the intervening time,
         # since the connection may have been lost in the meantime.
         time_of_last_success = now
